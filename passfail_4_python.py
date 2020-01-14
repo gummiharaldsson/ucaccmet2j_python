6f2ec2958be28421fdaadd6783aad4380354fc05
Gudmundur Haraldsson <gummihal@gmail.com>
2020-01-14 15:46:36 +0100

    Sum of precipitation over the whole year

--- passfail_4_python.py
+++ passfail_4_python.py
@@ -3,8 +3,16 @@ import json
 with open('precipitation.json', encoding='utf8') as file:
      data = json.load(file)
 
-# selecting only station seattle:
+# Part 2: calculating the sum of the precipitation over the whole year
 station_number = 'US1WAKG0038'
+
+total_precipitation = 0
+
 for datum in data:
     if datum['station'] == 'GHCND:US1WAKG0038':
-        print(datum)
+        precipitation_value = datum['value']
+        print(precipitation_value)
+        total_precipitation = total_precipitation + precipitation_value
+print(total_precipitation)
+# Answer is 11180
+
