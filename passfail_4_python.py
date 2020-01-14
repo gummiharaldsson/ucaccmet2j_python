81c8db65037dfa430f642f5d43c124d495bff64c
Gudmundur Haraldsson <gummihal@gmail.com>
2020-01-14 16:57:17 +0100

    Adding precipitation per month

--- passfail_4_python.py
+++ passfail_4_python.py
@@ -6,13 +6,23 @@ with open('precipitation.json', encoding='utf8') as file:
 # Part 2: calculating the sum of the precipitation over the whole year
 station_number = 'US1WAKG0038'
 
+# Total precipitation for whole year and per month
 total_precipitation = 0
+precipitation_per_month = [0]*12
 
 for datum in data:
     if datum['station'] == 'GHCND:US1WAKG0038':
+        
         precipitation_value = datum['value']
-        print(precipitation_value)
         total_precipitation = total_precipitation + precipitation_value
+
+        date = datum['date']
+        month = int(date[5:7])
+        rainfall = datum['value']
+        precipitation_per_month[month-1] += rainfall
+
+print(precipitation_per_month)
 print(total_precipitation)
+# [1693, 730, 870, 752, 924, 601, 54, 104, 996, 908, 1192, 2356]
 # Answer is 11180
 
